458b3c7effe843c987a555ba0b281345f62104aa
Patrik Arlos <patrik.arlos@gmail.com>
2022-05-02 12:09:31 +0200

    Update assignment2.py
    keeping time and string on one line.

--- assignment2.py
+++ assignment2.py
@@ -10,4 +10,4 @@ app = flask.Flask(__name__)
 @app.route('/')
 def index():
     Time= time.strftime("%H:%M:%S")
-    return Time+"\n Serving from "+h_name+" ("+IP_addres+")\n"
+    return Time+" Serving from "+h_name+" ("+IP_addres+")\n"
