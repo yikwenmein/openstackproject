7a8c3cfb3c82ee91e40f924517e744b6e645fb04
yikwenmein <yikwenmeinvictor1995@gmail.com>
2024-05-19 20:13:38 +0200

    first commit

---
+++ service.py
@@ -0,0 +1,22 @@
+import flask 
+import socket
+import time
+import random
+
+h_name = socket.gethostname()
+IP_addres = socket.gethostbyname(h_name)
+app = flask.Flask(__name__)
+
+@app.route('/')
+def index():
+    host = IP_addres
+    client_ip = flask.request.remote_addr
+    client_port = str(flask.request.environ.get('REMOTE_PORT'))
+    hostname = h_name
+    Time= time.strftime("%H:%M:%S")
+    rand=str(random.randint(0,100))
+    return Time+" "+client_ip + ":" +client_port +" -- " + host+" ("+hostname+") " +rand+"\n"
+
+if __name__ == '__main__':
+  app.run(host = '0.0.0.0', port = 5000, debug = True)
+  #app.run( port = 8080, debug = True)
