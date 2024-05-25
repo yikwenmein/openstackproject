f41ddf4f5115ae2ac101602635f1454fffdd72f8
yikwenmein <yikwenmeinvictor1995@gmail.com>
2024-05-25 13:19:40 +0200

    05/26

--- service.py
+++ service.py
@@ -1,22 +1,16 @@
-import flask 
-import socket
+import flask
 import time
-import random
+import socket
 
 h_name = socket.gethostname()
 IP_addres = socket.gethostbyname(h_name)
+
 app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    host = IP_addres
-    client_ip = flask.request.remote_addr
-    client_port = str(flask.request.environ.get('REMOTE_PORT'))
-    hostname = h_name
     Time= time.strftime("%H:%M:%S")
-    rand=str(random.randint(0,100))
-    return Time+" "+client_ip + ":" +client_port +" -- " + host+" ("+hostname+") " +rand+"\n"
+    return "Time---> "+Time+ "\n"+ "hostname---> "+h_name+" ("+IP_addres+")\n"
 
 if __name__ == '__main__':
-  app.run(host = '0.0.0.0', port = 5000, debug = True)
-  #app.run( port = 8080, debug = True)
+    app.run(host = '0.0.0.0', port = 80, debug = True)
